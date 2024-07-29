20e0c0cb1a10d4f1fb5d41779e5d179bcde2a901
kaneko10 <jinziyounan886@gmail.com>
2024-07-29 15:06:35 +0900

    グラフの色と太さの項目を追加

--- csv_to_json.py
+++ csv_to_json.py
@@ -11,24 +11,39 @@ df = df.fillna("NaN")
 # フレーム番号を抽出して整数に変換して"labels"に設定
 labels = df['Image'].apply(lambda x: int(x.split('_')[1].split('.')[0])).tolist()
 
+# 色と透明度の設定
+emotion_colors = {
+    "Surprise": "rgba(173, 216, 230, 0.4)",       # ライトブルー
+    "Fear": "rgba(128, 0, 128, 0.4)",           # 紫色
+    "Disgust": "rgba(85, 107, 47, 0.4)",        # ダークオリーブグリーン
+    "Happiness": "rgba(255, 165, 0, 0.4)",      # オレンジ
+    "Sadness": "rgba(0, 0, 255, 0.4)",          # 青
+    "Anger": "rgba(255, 0, 0, 0.4)",            # 赤
+    "Neutral": "rgba(128, 128, 128, 0.4)"       # グレー
+}
+
 # P_i, N_i, F_i のデータを含むJSONデータ作成
 pni_fi_data = {
     "labels": labels,
     "datasets": [
         {
             "label": col,
-            "data": df[col].tolist()
+            "data": df[col].tolist(),
+            "borderWidth": 2
         } for col in ['P_i', 'N_i', 'F_i'] if col in df.columns
     ]
 }
 
+# logit_ で始まるデータを含むJSONデータ作成
 # logit_ で始まるデータを含むJSONデータ作成
 logit_data = {
     "labels": labels,
     "datasets": [
         {
             "label": col,
-            "data": df[col].tolist()
+            "data": df[col].tolist(),
+            "borderColor": emotion_colors[col.split('_')[1]],
+            "borderWidth": 2
         } for col in df.columns if col.startswith('logit_')
     ]
 }
