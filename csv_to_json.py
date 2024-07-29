5f3e8dca1781e29ea31445fc5999fc73450af346
kaneko10 <jinziyounan886@gmail.com>
2024-07-29 12:47:37 +0900

    ヘッダー名で分割してJSON出力

--- csv_to_json.py
+++ csv_to_json.py
@@ -11,20 +11,36 @@ df = df.fillna("NaN")
 # フレーム番号を抽出して整数に変換して"labels"に設定
 labels = df['Image'].apply(lambda x: int(x.split('_')[1].split('.')[0])).tolist()
 
-# JSON形式のデータ作成
-json_data = {
+# P_i, N_i, F_i のデータを含むJSONデータ作成
+pni_fi_data = {
     "labels": labels,
     "datasets": [
         {
             "label": col,
             "data": df[col].tolist()
-        } for col in df.columns if col != 'Image'
+        } for col in ['P_i', 'N_i', 'F_i'] if col in df.columns
+    ]
+}
+
+# logit_ で始まるデータを含むJSONデータ作成
+logit_data = {
+    "labels": labels,
+    "datasets": [
+        {
+            "label": col,
+            "data": df[col].tolist()
+        } for col in df.columns if col.startswith('logit_')
     ]
 }
 
 # JSONファイルに保存
-json_file_path = 'output.json'
-with open(json_file_path, 'w') as json_file:
-    json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+pni_fi_json_path = 'pni_fi_output.json'
+logit_json_path = 'logit_output.json'
 
-print(f'Data has been successfully converted and saved to {json_file_path}')
+with open(pni_fi_json_path, 'w') as json_file:
+    json.dump(pni_fi_data, json_file, ensure_ascii=False, indent=4)
+
+with open(logit_json_path, 'w') as json_file:
+    json.dump(logit_data, json_file, ensure_ascii=False, indent=4)
+
+print(f'Data has been successfully converted and saved to {pni_fi_json_path} and {logit_json_path}')
