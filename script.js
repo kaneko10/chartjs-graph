042b54c481c3029b0b4c904dd6105738fa15bc1e
kaneko10 <jinziyounan886@gmail.com>
2024-07-29 19:24:31 +0900

    chartのIDを変更

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ fetch('logit_output.json')
             }
         };
 
-        var ctx = document.getElementById("chart").getContext("2d");
+        var ctx = document.getElementById("myChart").getContext("2d");
         var chart = new Chart(ctx, {
             type: 'line',
             data: data,
