d56aeae71e545f37eccedcde9e52dd615ccf1104
kaneko10 <jinziyounan886@gmail.com>
2024-08-02 13:42:26 +0900

    ファイルリストを人ごとにグループ化

--- file-list.js
+++ file-list.js
@@ -7,35 +7,65 @@ document.getElementById("file").addEventListener("change", (ev) => {
         filenames.push(file.name);
     }
 
-    // チェックボックスの設定
-    var listDiv= document.getElementById('file-list');
+    // グループを格納するオブジェクト
+    var groupedFiles = {};
 
     filenames.forEach(function (filename, index) {
-        // チェックボックスとラベルを一緒にするコンテナ
-        var container = document.createElement('div');
-        container.className = 'file-item';
+        // 拡張子が.jsonでないファイルは無視する
+        if (!filename.endsWith('.json')) return;
 
-        var checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.name = 'filelist';
-        checkbox.value = index;
-        checkbox.id = 'checkbox-file-' + index;
+        // 最後の"_"以降の部分を取り出し、拡張子を除く
+        const groupKey = filename.split('_').slice(-1)[0].replace('.json', '');
 
-        var labelElement = document.createElement('label');
-        labelElement.htmlFor = 'checkbox-file-' + index;
-        labelElement.innerText = filename;
+        // グループが存在しなければ作成する
+        if (!groupedFiles[groupKey]) {
+            groupedFiles[groupKey] = [];
+        }
 
-        checkbox.addEventListener('change', function () {
-            if (checkbox.checked) {
-                const filepath = 'json/' + filenames[index];
-                drawGraph(filepath, index);
-            } else {
-                removeGraph(index);
-            }
+        // グループにファイル名を追加する
+        groupedFiles[groupKey].push(filename);
+    });
+
+    // チェックボックスの設定
+    var listDiv = document.getElementById('file-list');
+
+    // 各グループごとに処理を行う
+    Object.keys(groupedFiles).forEach(groupKey => {
+        const files = groupedFiles[groupKey];
+        console.log(`Processing group: ${groupKey}`);
+        // チェックボックス用のdiv
+        var checkboxDiv = document.createElement('div');
+        checkboxDiv.className = 'checkboxDiv';
+
+        files.forEach(function (filename, index) {
+            console.log(`  - ${filename}`);
+            // チェックボックスとラベルを一緒にするコンテナ
+            var container = document.createElement('div');
+            container.className = 'file-item';
+
+            var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.name = groupKey;
+            checkbox.value = index;
+            checkbox.id = 'checkbox-file-' + groupKey + index;
+
+            var labelElement = document.createElement('label');
+            labelElement.htmlFor = 'checkbox-file-' + groupKey + index;
+            labelElement.innerText = filename;
+
+            checkbox.addEventListener('change', function () {
+                if (checkbox.checked) {
+                    const filepath = 'json/' + filename;
+                    drawGraph(filepath, `${groupKey}${index}`);
+                } else {
+                    removeGraph(`${groupKey}${index}`);
+                }
+            });
+
+            container.appendChild(checkbox);
+            container.appendChild(labelElement);
+            checkboxDiv.appendChild(container);
         });
-
-        container.appendChild(checkbox);
-        container.appendChild(labelElement);
-        listDiv.appendChild(container);
+        listDiv.appendChild(checkboxDiv);
     });
 });
