1421d7637d5f4333146de95441d8aef5fadb5378
kaneko10 <jinziyounan886@gmail.com>
2024-08-13 13:32:59 +0900

    csv中の複数のデータを1つのjsonに統合

--- csv_to_json.py
+++ csv_to_json.py
@@ -60,6 +60,15 @@ for csv_file in csv_files:
         df1 = pd.DataFrame([ps], columns=other_columns)
         df_other.iloc[i] = df1.iloc[0]
 
+    emotion_colums = [col for col in df.columns if col in ['Emotion_ind', 'Emotion']]
+    df_emotion = pd.DataFrame(index=range(df.shape[0]), columns=emotion_colums)
+    for i in range(df.shape[0]):
+        ps = df.iloc[i].loc["Emotion_ind":"Emotion"]
+        if pd.isna(ps[0]):
+            ps = [None] * len(emotion_colums)   # Noneの部分はNoneで埋める
+        df1 = pd.DataFrame([ps], columns=emotion_colums)
+        df_emotion.iloc[i] = df1.iloc[0]
+
     # P_i, N_i のデータを含むJSONデータ作成
     pi_ni_data = {
         "labels": labels,
@@ -76,19 +85,19 @@ for csv_file in csv_files:
     }
 
     # F_i のデータを含むJSONデータ作成
-    fi_data = {
-        "labels": labels,
-        "datasets": [
-            {
-                "label": col,
-                "data": df_other[col].tolist(),
-                "borderColor": emotion_colors[col],
-                "backgroundColor": emotion_colors[col].replace('0.4', '0.2'),
-                "borderWidth": 2,
-                "pointRadius": 1,
-            } for col in other_columns if col in ['F_i']
-        ]
-    }
+    # fi_data = {
+    #     "labels": labels,
+    #     "datasets": [
+    #         {
+    #             "label": col,
+    #             "data": df_other[col].tolist(),
+    #             "borderColor": emotion_colors[col],
+    #             "backgroundColor": emotion_colors[col].replace('0.4', '0.2'),
+    #             "borderWidth": 2,
+    #             "pointRadius": 1,
+    #         } for col in other_columns if col in ['F_i']
+    #     ]
+    # }
 
     # logit_ で始まるデータを含むJSONデータ作成
     logit_data = {
@@ -106,37 +115,56 @@ for csv_file in csv_files:
     }
 
     # スムージング
-    window_size = 10    # fps: 30
-    logit_smooth_data = {
+    # window_size = 10    # fps: 30
+    # logit_smooth_data = {
+    #     "labels": labels,
+    #     "datasets": [
+    #         {
+    #             "label": f'{col}_smooth',
+    #             "data": pd.Series(df_logit[col]).rolling(window=window_size).mean().fillna(0).tolist(), # NaNは0で置き換える
+    #             "borderColor": emotion_colors[col.split('_')[1]],
+    #             "backgroundColor": emotion_colors[col.split('_')[1]].replace('0.4', '0.2'),
+    #             "borderWidth": 2,
+    #             "pointRadius": 1,
+    #         } for col in logit_columns
+    #     ]
+    # }
+
+    emotion_data = {
         "labels": labels,
         "datasets": [
             {
-                "label": f'{col}_smooth',
-                "data": pd.Series(df_logit[col]).rolling(window=window_size).mean().fillna(0).tolist(), # NaNは0で置き換える
-                "borderColor": emotion_colors[col.split('_')[1]],
-                "backgroundColor": emotion_colors[col.split('_')[1]].replace('0.4', '0.2'),
-                "borderWidth": 2,
-                "pointRadius": 1,
-            } for col in logit_columns
+                "label": col,
+                "data": df_emotion[col].tolist(),
+            } for col in emotion_colums
         ]
     }
 
+    full_data = {
+        "labels": logit_data["labels"],
+        "datasets": logit_data["datasets"] + pi_ni_data["datasets"] + emotion_data["datasets"]
+    }
+
     # JSONファイルに保存
-    pi_ni_json_path = f'json/pi_ni_{base_name}.json'
-    fi_json_path = f'json/fi_{base_name}.json'
-    logit_json_path = f'json/logit_{base_name}.json'
-    logit_json_smooth_path = f'json/logit_smooth_{base_name}.json'
+    # pi_ni_json_path = f'json/pi_ni_{base_name}.json'
+    # fi_json_path = f'json/fi_{base_name}.json'
+    # logit_json_path = f'json/logit_{base_name}.json'
+    # logit_json_smooth_path = f'json/logit_smooth_{base_name}.json'
+    full_json_path = f'json/full_{base_name}.json'
 
-    with open(pi_ni_json_path, 'w') as json_file:
-        json.dump(pi_ni_data, json_file, ensure_ascii=False, indent=4)
+    # with open(pi_ni_json_path, 'w') as json_file:
+    #     json.dump(pi_ni_data, json_file, ensure_ascii=False, indent=4)
 
-    with open(fi_json_path, 'w') as json_file:
-        json.dump(fi_data, json_file, ensure_ascii=False, indent=4)
+    # with open(fi_json_path, 'w') as json_file:
+    #     json.dump(fi_data, json_file, ensure_ascii=False, indent=4)
 
-    with open(logit_json_path, 'w') as json_file:
-        json.dump(logit_data, json_file, ensure_ascii=False, indent=4)
+    # with open(logit_json_path, 'w') as json_file:
+    #     json.dump(logit_data, json_file, ensure_ascii=False, indent=4)
 
-    with open(logit_json_smooth_path, 'w') as json_file:
-        json.dump(logit_smooth_data, json_file, ensure_ascii=False, indent=4)
+    # with open(logit_json_smooth_path, 'w') as json_file:
+    #     json.dump(logit_smooth_data, json_file, ensure_ascii=False, indent=4)
 
-    print(f'Data has been successfully converted and saved to {pi_ni_json_path} and {logit_json_path}')
+    with open(full_json_path, 'w') as json_file:
+        json.dump(full_data, json_file, ensure_ascii=False, indent=4)
+
+    print(f'Data has been successfully converted and saved: {csv_file} -> json')
