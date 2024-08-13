adda2ea624155ccf1cdbf77f6b832f631621c3b8
kaneko10 <jinziyounan886@gmail.com>
2024-08-13 16:58:10 +0900

    ファイル名のグループ化処理を削除

--- graph-items.js
+++ graph-items.js
@@ -1,44 +1,45 @@
-function makeGraphItems(groupedFiles) {
+function makeGraphItems(filenames) {
     // チェックボックスの設定
     var listDiv = document.getElementById('file-list');
 
     // 各グループごとに処理を行う
-    Object.keys(groupedFiles).forEach(groupKey => {
-        const files = groupedFiles[groupKey];
+    filenames.forEach(function (filename, index) {
+        // 最後の"_"以降の部分を取り出し、拡張子を除く
+        const groupKey = filename.split('_').slice(-1)[0].replace('.json', '');
+
         // チェックボックス用のdiv
         var checkboxDiv = document.createElement('div');
         checkboxDiv.className = 'checkboxDiv';
 
-        files.forEach(function (filename, index) {
-            // チェックボックスとラベルを一緒にするコンテナ
-            var container = document.createElement('div');
-            container.className = 'file-item';
+        // チェックボックスとラベルを一緒にするコンテナ
+        var container = document.createElement('div');
+        container.className = 'file-item';
 
-            var checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.name = groupKey;
-            checkbox.value = index;
-            checkbox.id = 'checkbox-file-' + groupKey + index;
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = groupKey;
+        checkbox.value = index;
+        checkbox.id = 'checkbox-file-' + groupKey + index;
 
-            var labelElement = document.createElement('label');
-            labelElement.htmlFor = 'checkbox-file-' + groupKey + index;
-            labelElement.innerText = filename;
+        var labelElement = document.createElement('label');
+        labelElement.htmlFor = 'checkbox-file-' + groupKey + index;
+        labelElement.innerText = filename;
 
-            checkbox.addEventListener('change', function () {
-                if (checkbox.checked) {
-                    const filepath = `json/${filename}`
-                    const graphID = `${groupKey}${index}`
-                    drawGraph(filepath, graphID);
-                } else {
-                    const graphID = `${groupKey}${index}`
-                    removeGraph(graphID);
-                }
-            });
-
-            container.appendChild(checkbox);
-            container.appendChild(labelElement);
-            checkboxDiv.appendChild(container);
+        checkbox.addEventListener('change', function () {
+            if (checkbox.checked) {
+                const filepath = `json/${filename}`
+                const graphID = `${groupKey}${index}`
+                drawGraph(filepath, graphID);
+            } else {
+                const graphID = `${groupKey}${index}`
+                removeGraph(graphID);
+            }
         });
+
+        container.appendChild(checkbox);
+        container.appendChild(labelElement);
+        checkboxDiv.appendChild(container);
+        
         listDiv.appendChild(checkboxDiv);
     });
 }
