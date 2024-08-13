7e4dc3eb024ed321f7a337e2b7078089619b5580
kaneko10 <jinziyounan886@gmail.com>
2024-08-13 12:55:34 +0900

    各人のアノテーションファイルからラベル付与

--- annotation_json.py
+++ annotation_json.py
@@ -12,10 +12,6 @@ def extract_name(file_name):
     name = base_name[last_underscore_index + 1:]
     return name
 
-# 特定のnameに基づいてデータをフィルタリングする関数
-def filter_data_by_name(name, df):
-    return df[df['name'] == name]
-
 # 範囲内の判定を行う関数
 def check_range(value, data):
     for index, row in data.iterrows():
@@ -25,40 +21,46 @@ def check_range(value, data):
             return row['action']
     return None
 
+# json_files = glob.glob(os.path.join('json/', '*.json'))
+# json_files = [os.path.basename(file) for file in json_files]
+
+file_name = 'logit_seq_OpenCV_rec_expt1_XXX.json'
+json_file_path = f'json/{file_name}'
+name = extract_name(file_name)
+
 # CSVファイルの読み込み
-file_path = './annotations/annotation.csv'
-df = pd.read_csv(file_path)
+csv_file_path = f'./annotations/annotation_{name}.csv'
+df = pd.read_csv(csv_file_path)
 
-json_files = glob.glob(os.path.join('json/', '*.json'))
-json_files = [os.path.basename(file) for file in json_files]
+try:
+    # JSONファイルを読み込む
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
 
-for file_name in json_files:
-    print(file_name)
-    file_path = f'json/{file_name}'
-    try:
-        # JSONファイルを読み込む
-        with open(file_path, 'r') as file:
-            data = json.load(file)
+    annotaion_data = []
 
-        annotaion_data = []
+    filtered_data = None
+    if 'expt1' in file_name:
+        print('expt1')
+        filtered_data = df[df['type'] == 'expt1']
+    elif 'expt2' in file_name:
+        print('expt2')
+        filtered_data = df[df['type'] == 'expt2']
 
-        for label in data['labels']:
-            # 判定する数値を指定
-            value_to_check = label
-            # 範囲をチェック
-            name_to_check = extract_name(file_name)
-            filtered_data = filter_data_by_name(name_to_check, df)
-            action = check_range(value_to_check, filtered_data)
-            annotaion_data.append(action)
-        
-        data['annotations'] = annotaion_data
+    for label in data['labels']:
+        # 判定する数値を指定
+        value_to_check = label
+        action = check_range(value_to_check, filtered_data)
+        annotaion_data.append(action)
+    
+    data['annotations'] = annotaion_data
 
-        with open(file_path, 'w') as file:
-            json.dump(data, file, indent=4)
-        print("New item added to JSON file.")
-    except FileNotFoundError:
-        print(f"Error: The file {file_path} was not found.")
-    except json.JSONDecodeError:
-        print(f"Error: The file {file_path} is not valid JSON.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    with open(json_file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+    print("New item added to JSON file.")
+except FileNotFoundError:
+    print(f"Error: The file {json_file_path} was not found.")
+except json.JSONDecodeError:
+    print(f"Error: The file {json_file_path} is not valid JSON.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
